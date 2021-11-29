66aba7c177692fdfe087845494010e4f3a19f636
Jason Ouyang <jasonouyang80@gmail.com>
2021-11-29 02:35:32 -0800

    add create root feature from react-18

--- react-18-features/src/index.js
+++ react-18-features/src/index.js
@@ -1,6 +1,10 @@
-import {render} from 'react-dom'
+import {createRoot} from 'react-dom'
 import App from './App'
 
-render(<App />, document.getElementById('app'))
+const container = document.getElementById('app')
+// Create a root.
+const root = createRoot(container)
+// Render the top component to the root.
+root.render(<App />)
 
 module.hot.accept()
