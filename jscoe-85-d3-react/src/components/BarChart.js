0b9aa23d3a81e4d63b5a0f2df4c91ec9963cec4d
Jason Ouyang <jasonouyang80@gmail.com>
2022-01-02 23:00:41 -0800

    add useRef to manipulate dom

--- jscoe-85-d3-react/src/components/BarChart.js
+++ jscoe-85-d3-react/src/components/BarChart.js
@@ -1,5 +1,21 @@
+import d3 from 'd3'
+import { useEffect, useRef } from 'react'
+
 export default function BarGraph() {
+    const ref = useRef()
+
+    useEffect(() => {
+        console.log(ref)
+    }, [])
+
     return (
+        <>
         <div>BarChart</div>
+        <svg ref={ref}>
+            <g className="plot-area" />
+            <g className="x-axis" />
+            <g className="y-axis" />
+        </svg>
+        </>
     )
 }
