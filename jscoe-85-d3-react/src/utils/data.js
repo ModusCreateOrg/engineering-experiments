f6ef7a58fc444d8b0091181b5395caa87ce4a6a6
Jason Ouyang <jasonouyang80@gmail.com>
2022-01-02 21:17:12 -0800

    add create Data util

---
+++ jscoe-85-d3-react/src/utils/data.js
@@ -0,0 +1,16 @@
+import { randomNumber } from "./utils";
+
+export const createData = (amount, config) => {
+    const {xMax, xMin, yMax, yMin} = config;
+
+    const dataPoints = [];
+
+    for (let i = 1; i <= amount; i++) {
+        dataPoints.push({
+            x: randomNumber(xMin, xMax),
+            y: randomNumber(yMin, yMax)
+        })
+    }   
+
+    return dataPoints
+}
---
+++ jscoe-85-d3-react/src/utils/utils.js
@@ -0,0 +1,4 @@
+export const randomNumber = (min, max) => { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  
