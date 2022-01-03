01c51cba99d854a2b480db6e2b44207764a4c172
Jason Ouyang <jasonouyang80@gmail.com>
2022-01-02 21:11:40 -0800

    set background

--- jscoe-85-d3-react/src/App.js
+++ jscoe-85-d3-react/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>D3 Visualizations</h1>
+        <h1 className="text-center">D3 Visuals</h1>
+        <div className="container">
+            
+
+        </div>
     </div>
   );
 }
