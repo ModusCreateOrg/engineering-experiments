ba472838ddde9274398b38ad6148bf5cb4ce0408
Vijey Narayanaswamy <vijeysrc@gmail.com>
2022-01-11 15:31:25 +0530

    feat: style changes

--- jscoe-88-module-federation-with-webpack/packages/base-app/src/components/Header/Header.jsx
+++ jscoe-88-module-federation-with-webpack/packages/base-app/src/components/Header/Header.jsx
@@ -11,6 +11,12 @@ const Header = () => {
   return (
     <div className="main-header">
       <div className="logo-section">
+        <img
+          src="https://3lhowb48prep40031529g5yj-wpengine.netdna-ssl.com/wp-content/uploads/2018/11/avatar_user_57_1542388114.png"
+          alt=""
+          height="80"
+          width="80"
+        />
         <Link to="/">Modus Create</Link>
       </div>
       <ul className="nav-items">
