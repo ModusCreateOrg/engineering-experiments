b3ee5c52db3854dcdfba1d541356bc4513cdfb28
chiefoleka <valentinenwakibeya@gmail.com>
2022-01-13 19:14:13 +0000

    [JSCOE-90] Get invoice HTTP ready

--- grpc-server-to-server-communication/experiment/invoices/src/index.ts
+++ grpc-server-to-server-communication/experiment/invoices/src/index.ts
@@ -1,10 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import { HttpController } from './controllers';
+import config from './shared/configs';
 
-const app    = express();
-const port   = 8002;
-const router = express.Router();
+const app            = express();
+const router         = express.Router();
+const port           = config.ports.self;
+const httpController = new HttpController();
 
-app.get('/', (req, res) => res.send('We are live with invoices'));
+// Actual APIs
+router.post('/', httpController.create.bind(httpController));
+router.get('/:id', httpController.getOne.bind(httpController));
+router.put('/:id', httpController.update.bind(httpController));
 
-app.listen(port, () => console.log(`Server is listening on ${port}`));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1/invoices', router);
+
+app.listen(port, () => console.log(`server is listening on ${port}`));
+
