e5f403b256e149368731415ccdab358a6059cca6
chiefoleka <valentinenwakibeya@gmail.com>
2022-01-13 19:12:02 +0000

    [JSCOE-90] Now we are ready to access events via HTTP

--- grpc-server-to-server-communication/experiment/events/src/index.ts
+++ grpc-server-to-server-communication/experiment/events/src/index.ts
@@ -1,10 +1,26 @@
 import express from 'express';
 import cors from 'cors';
+import { HttpController } from './controllers';
+import config from './shared/configs';
 
-const app    = express();
-const port   = 8001;
-const router = express.Router();
+const app            = express();
+const router         = express.Router();
+const port           = config.ports.self;
+const httpController = new HttpController();
 
-app.get('/', (req, res) => res.send('We are live with events'));
+// Actual HTTP APIs
+router.get('/', httpController.getAll.bind(httpController));
+router.post('/', httpController.create.bind(httpController));
+router.get('/:id', httpController.getOne.bind(httpController));
 
-app.listen(port, () => console.log(`Server is listening on ${port}`));
+router.get('/:id/tickets', httpController.getTickets.bind(httpController));
+router.post('/:id/tickets', httpController.buyTicket.bind(httpController));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1/events', router);
+
+app.listen(port, () => console.log(`HTTP server is listening on ${port}`));
+
