82fe711abe81b40f8d539fd32e4e20e401c73362
chiefoleka <valentinenwakibeya@gmail.com>
2022-01-13 18:46:34 +0000

    [JSCOE-90] Adjust config for defining http connections

--- grpc-server-to-server-communication/experiment/events/src/shared/configs/index.ts
+++ grpc-server-to-server-communication/experiment/events/src/shared/configs/index.ts
@@ -3,6 +3,15 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+// Handling the misconfiguration of services
+if (!process.env.DNS) {
+  throw new Error('No domain configured for server');
+}
+
+if (!process.env.PORT_SELF) {
+  throw new Error('No port configured for server');
+}
+
 const dbConfig: DBConfigInterface = Object.create(null);
 const dbParams = ['name', 'host', 'user', 'pass'];
 
@@ -18,6 +27,12 @@ for (const param of dbParams) {
 }
 
 const config: ServerInterface = {
+  ports: {
+    self: process.env.PORT_SELF,
+    invoices: process.env.PORT_INVOICES || '',
+    tickets: process.env.PORT_TICKETS || '',
+  },
+  dns: process.env.DNS,
   db: dbConfig,
   isDevelopment: ['development', 'dev', 'local'].includes(process.env.NODE_ENV || 'dev')
 };
