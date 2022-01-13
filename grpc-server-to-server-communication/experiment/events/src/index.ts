2bd87767b46d3932f534a18d5fde798a9bf20286
chiefoleka <valentinenwakibeya@gmail.com>
2022-01-13 20:29:34 +0000

    [JSCOE-90] Complete the gRPC server for events

--- grpc-server-to-server-communication/experiment/events/src/index.ts
+++ grpc-server-to-server-communication/experiment/events/src/index.ts
@@ -1,7 +1,11 @@
+import * as path from 'path';
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
 import express from 'express';
 import cors from 'cors';
 import { HttpController } from './controllers/http';
 import config from './shared/configs';
+import { GrpcController } from './controllers/grpc';
 
 const app            = express();
 const router         = express.Router();
@@ -24,3 +28,26 @@ app.use('/api/v1/events', router);
 
 app.listen(httpPort, () => console.log(`HTTP server is listening on ${httpPort}`));
 
+// GRPC Server
+const server         = new grpc.Server();
+const packageDef     = protoLoader.loadSync(path.join(__dirname, "proto/event.proto"));
+const grpcObject     = grpc.loadPackageDefinition(packageDef);
+const eventPackage   = grpcObject.eventPackage;
+const grpcController = new GrpcController();
+
+// Plain text. Using createSsl requires providing ssl credentials
+server.bindAsync(config.grpcDNS.self, grpc.ServerCredentials.createInsecure(), (err, port) => {
+  if (err) throw err;
+
+  console.log(`gRPC server is listening on ${port}`);
+  server.start();
+});
+
+// @ts-ignore: This is a valid code that works
+server.addService(eventPackage.Event.service, {
+  createEvent: grpcController.create.bind(grpcController),
+  readEvent: grpcController.getOne.bind(grpcController),
+  readEvents: grpcController.getAll.bind(grpcController),
+  getTickets: grpcController.getTickets.bind(grpcController),
+  buyTicket: grpcController.buyTicket.bind(grpcController)
+});
