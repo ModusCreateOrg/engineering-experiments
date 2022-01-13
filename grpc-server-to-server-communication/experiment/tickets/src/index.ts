c9e724e82003bbf7bd7314bcb39754a62f8ea5f6
chiefoleka <valentinenwakibeya@gmail.com>
2022-01-13 19:16:40 +0000

    [JSCOE-90] Tickets server is HTTP ready

--- grpc-server-to-server-communication/experiment/tickets/src/index.ts
+++ grpc-server-to-server-communication/experiment/tickets/src/index.ts
@@ -1,10 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import { HttpController } from './controllers';
+import config from './shared/configs';
 
-const app    = express();
-const port   = 8003;
-const router = express.Router();
+const app            = express();
+const router         = express.Router();
+const port           = config.ports.self;
+const httpController = new HttpController();
 
-app.get('/', (req, res) => res.send('We are live with tickets'));
+// Actual APIs
+router.get('/', httpController.getAll.bind(httpController));
+router.post('/', httpController.create.bind(httpController));
 
-app.listen(port, () => console.log(`Server is listening on ${port}`));
+router.get('/:id', httpController.getOne.bind(httpController));
+router.get('/:id/attendee', httpController.getAttendeeInfo.bind(httpController));
+router.get('/:id/event', httpController.getEventInfo.bind(httpController ));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1/tickets', router);
+
+app.listen(port, () => console.log(`server is listening on ${port}`));
