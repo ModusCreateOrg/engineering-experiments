b52868812b0939543163e874f280a712ca39a64b
Jason Ouyang <jasonouyang80@gmail.com>
2022-02-02 15:32:34 -0800

    import homepage to App.svelete

--- JSCOE-132/src/App.svelte
+++ JSCOE-132/src/App.svelte
@@ -1,4 +1,6 @@
 <script>
-
+	import HomePage from "./Pages/HomePage.svelte"
+	import Navbar from "./Components/Navbar.svelte";
 </script>
 
+<HomePage/>
---
+++ JSCOE-132/src/Pages/Homepage.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+<h1>HomePage for Learning Svelte</h1>
+
+<!-- Add styles just for Home Page -->
+<style>
+    /* Global Styles */
+    :global(body) {
+		/* this will apply to <body> */
+		margin: 0;
+        padding: 0;
+	}
+
+    h1 {
+        text-align: center;
+    }
+</style>
