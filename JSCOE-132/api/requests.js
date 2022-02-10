7ea65c4bd5d638e190bc444bc3cd39c30f6b9d93
Jason Ouyang <jasonouyang80@gmail.com>
2022-02-09 18:01:29 -0800

    include api with fetch rendering from svlete

---
+++ JSCOE-132/api/requests.js
@@ -0,0 +1,6 @@
+export const getPictureOfTheDay = async () => {
+   const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=joZbunAb7UdEqNv9V6kd7AxvcLuc0XpzjOcBdg0h');
+   const json = await res.json();
+
+   return json;
+}
--- JSCOE-132/src/Pages/ApiPage.svelte
+++ JSCOE-132/src/Pages/ApiPage.svelte
@@ -1,13 +1,25 @@
 <script>
+import { getPictureOfTheDay } from "../../api/requests";
 
-  </script>
+</script>
   
+
   <h1>Api Page</h1>
   <p>Here we will best testing the api.</p>
-  
+  {#await getPictureOfTheDay()}
+    <p>Loading...</p>
+  {:then data} 
+    <h3>{data.date}</h3>
+    <h3>{data.title}</h3>
+    <p>{data.explanation}</p>
+  {/await}
   <style>
     h1 {
       text-align: center;
     }
+    h3 {
+      text-align: center;
+    }
+
   </style>
   
