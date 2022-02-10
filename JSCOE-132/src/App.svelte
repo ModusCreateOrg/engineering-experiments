8016fc9adce073a70059faeff3b9e8c2860475af
Jason Ouyang <jasonouyang80@gmail.com>
2022-02-09 18:04:45 -0800

    prettier setup for svlte local dev env

--- JSCOE-132/src/App.svelte
+++ JSCOE-132/src/App.svelte
@@ -1,51 +1,50 @@
 <script>
-	import HomePage from "./Pages/HomePage.svelte";
-	import Navbar from "./Components/Navbar.svelte";
-	import {Router, Route} from "svelte-navigator";
-	import ApiPage from "./Pages/ApiPage.svelte";
+  import HomePage from "./Pages/HomePage.svelte";
+  import Navbar from "./Components/Navbar.svelte";
+  import { Router, Route } from "svelte-navigator";
+  import ApiPage from "./Pages/ApiPage.svelte";
 </script>
 
 <main>
-	<div class="container">
-		<Router>
-			<Navbar />
-			<Route path="/">
-				<HomePage/>
-			</Route>
-			<Route path="api">
-				<ApiPage/>
-			</Route>
-		</Router>
-
-	</div>
+  <div class="container">
+    <Router>
+      <Navbar />
+      <Route path="/">
+        <HomePage />
+      </Route>
+      <Route path="api">
+        <ApiPage />
+      </Route>
+    </Router>
+  </div>
 </main>
 
-
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+    rel="stylesheet"
+  />
 </svelte:head>
 
 <style>
-	:global(*) {
-		box-sizing: border-box;
-	}
+  :global(*) {
+    box-sizing: border-box;
+  }
 
-	:global(body) {
-		/* this will apply to <body> */
-		margin: 0;
-        padding: 0;
-        background: #1A1A40;
-		min-height: 100vh;
-        font-family: 'Poppins', sans-serif;
-		color: rgb(255, 253, 253);
+  :global(body) {
+    /* this will apply to <body> */
+    margin: 0;
+    padding: 0;
+    background: #1a1a40;
+    min-height: 100vh;
+    font-family: "Poppins", sans-serif;
+    color: rgb(255, 253, 253);
+  }
 
-	}
-
-	.container {
-		max-width: 1100px;
-		margin: 0px auto;
-	}
-
+  .container {
+    max-width: 1100px;
+    margin: 0px auto;
+  }
 </style>
--- JSCOE-132/src/Pages/ApiPage.svelte
+++ JSCOE-132/src/Pages/ApiPage.svelte
@@ -1,25 +1,22 @@
 <script>
-import { getPictureOfTheDay } from "../../api/requests";
-
+  import { getPictureOfTheDay } from "../../api/requests";
 </script>
-  
 
-  <h1>Api Page</h1>
-  <p>Here we will best testing the api.</p>
-  {#await getPictureOfTheDay()}
-    <p>Loading...</p>
-  {:then data} 
-    <h3>{data.date}</h3>
-    <h3>{data.title}</h3>
-    <p>{data.explanation}</p>
-  {/await}
-  <style>
-    h1 {
-      text-align: center;
-    }
-    h3 {
-      text-align: center;
-    }
+<h1>Api Page</h1>
+<p>Here we will best testing the api.</p>
+{#await getPictureOfTheDay()}
+  <p>Loading...</p>
+{:then data}
+  <h3>{data.date}</h3>
+  <h3>{data.title}</h3>
+  <p>{data.explanation}</p>
+{/await}
 
-  </style>
-  
+<style>
+  h1 {
+    text-align: center;
+  }
+  h3 {
+    text-align: center;
+  }
+</style>
