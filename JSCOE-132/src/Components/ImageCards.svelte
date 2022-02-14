0362905427067c4bc18c2930bb67682f2073d416
Jason Ouyang <jasonouyang80@gmail.com>
2022-02-14 02:10:22 -0800

    add cards

--- JSCOE-132/src/Components/ImageCards.svelte
+++ JSCOE-132/src/Components/ImageCards.svelte
@@ -1,5 +1,53 @@
 <script>
-    
 
+    import  { images } from '../Stores/Store'
+  
+    function handleChange(event, id) {
 
+        const copyImages= [...$images];
+        const idx = copyImages.findIndex(c => c.id === id);
+        copyImages[idx].title = event.target.value; 
+        $images = copyImages;
+
+    }
 </script>
+
+<div class="cars">
+	{#each $images as { title, image, id} }
+		<div class="outer-div">
+            <div>
+                <input type="text" on:change={(e) => handleChange(e, id)} value={title} />
+                <img src={image} alt={title} />
+            </div>
+		</div>
+	{/each}
+</div>
+
+<style>
+    input {
+        color: #000;
+        text-align: center;
+        font-size: 18px;
+        width: 100%;
+    }
+
+    .cars {
+        display: flex;
+    }
+    .outer-div {
+        padding: 10px;
+        width: 25%;
+    }
+
+    .outer-div div {
+        padding: 5px;
+        background: #fff;
+    }
+
+    img {
+        height: 300px;
+        width: 100%;
+        object-fit: cover;
+        padding: 5px;
+    }
+</style>
--- JSCOE-132/src/Pages/Homepage.svelte
+++ JSCOE-132/src/Pages/Homepage.svelte
@@ -2,6 +2,7 @@
   import Counter from "../Components/Counter.svelte";
   import CounterMessage from "../Components/CounterMessage.svelte";
   import { count } from '../Stores/Store.js';
+  import ImageCards from '../Components/ImageCards.svelte';
   let countNumber;
 
   count.subscribe(c => {
@@ -26,6 +27,7 @@
 <h1>Home Svelte</h1>
 <Counter count={countNumber} {increment} {decrement} />
 <CounterMessage count={countNumber} />
+<ImageCards />
 <style>
   h1 {
     text-align: center;
