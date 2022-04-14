4f23e06a9ed2b9f537fd02fcaa19b08f0cf6bee1
Muhammad Amir <mianamirlahore@gmail.com>
2022-04-15 03:06:00 +0500

    .env-sample added

--- slackbot_poc/slack_bot_poc.py
+++ slackbot_poc/slack_bot_poc.py
@@ -15,10 +15,11 @@ from date_parsers import (
 
 app = Flask(__name__)
 
-slack_event_adapter = SlackEventAdapter('bdca1a7d9bccfad92e22a5e6e6514ff2','/slack/events',app)
-
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
+
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
+
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 client.chat_postMessage(channel='#birthdays', text='Happy Birthday Muhammad!')
 BOT_ID = client.api_call('auth.test')['user_id']
